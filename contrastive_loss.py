23e0b3ee8290fa159b8ae300529819b962d164f6
double <mohamed.ahmed.geek@gmail.com>
2020-06-15 02:02:14 +0200

    CUB dataset

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -13,7 +13,6 @@ class ContrastiveLoss(torch.nn.Module):
         self.margin = margin
 
     def forward(self, output1, output2, label):
-        print(output1.size(),output2.size())
         euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
         loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
                                       (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
