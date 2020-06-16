6b543126ad7e6e861d41718a0f319e16ef7da77f
double <mohamed.ahmed.geek@gmail.com>
2020-06-16 21:51:43 +0200

    fashion mnist

--- CustomNet.py
+++ CustomNet.py
@@ -25,12 +25,12 @@ class CustomNet(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            # nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1),
+            # nn.BatchNorm2d(32),
+            # nn.ReLU(),
+            # nn.MaxPool2d(kernel_size=2),
 
-            nn.Conv2d(in_channels=32, out_channels=120, kernel_size=5, stride=1),
+            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5, stride=1),
             nn.BatchNorm2d(120),
             nn.ReLU(),
         )
