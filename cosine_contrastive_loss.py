51bc66f0a498c239c5ba99d35f11aacbf2393451
double <mohamed.ahmed.geek@gmail.com>
2020-06-16 20:06:12 +0200

    fashion mnist

--- cosine_contrastive_loss.py
+++ cosine_contrastive_loss.py
@@ -35,4 +35,5 @@ class CosineContrastiveLoss(nn.Module):
         # cos_sim = F.cosine_similarity(output1, output2)
         loss_cos_con = torch.mean((1-label) * torch.div(torch.pow((1.0-cos_sim), 2), 4) +
                                     (label) * torch.pow(cos_sim * torch.lt(cos_sim, self.margin).float(), 2))
+        print("loss:",loss_cos_con)
         return loss_cos_con
