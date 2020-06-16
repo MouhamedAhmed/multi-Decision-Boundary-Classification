0bb73b08cb0c7be0117fd5d9d67a87e32e7da3e7
double <mohamed.ahmed.geek@gmail.com>
2020-06-16 22:16:09 +0200

    fashion mnist

--- CustomNet.py
+++ CustomNet.py
@@ -15,20 +15,30 @@ class CustomNet(nn.Module):
     def __init__(self, n_classes):
         super(CustomNet, self).__init__()
         self.n = n_classes
-        self.feature_extractor = nn.Sequential(            
+        self.feature_extractor1 = nn.Sequential(            
             nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
+        )
 
+        self.feature_extractor2 = nn.Sequential(            
+           
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
+        )
 
+        self.feature_extractor3 = nn.Sequential(           
+            
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
+
+        )
+
+        self.feature_extractor4 = nn.Sequential(           
 
             nn.Conv2d(in_channels=32, out_channels=120, kernel_size=5, stride=1),
             nn.BatchNorm2d(120),
@@ -45,7 +55,14 @@ class CustomNet(nn.Module):
 
 
     def forward(self, x):
-        x = self.feature_extractor(x)
+        x = self.feature_extractor1(x)
+        print(x.size())
+        x = self.feature_extractor2(x)
+        print(x.size())
+        x = self.feature_extractor3(x)
+        print(x.size())
+        x = self.feature_extractor4(x)
+        print(x.size())
         x = torch.flatten(x, 1)
         l1 = self.classifier1(x)
         l2 = self.classifier2(l1)
