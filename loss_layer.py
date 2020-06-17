b04f9e9ec4705169e43b5aa8162398e59238e495
double <mohamed.ahmed.geek@gmail.com>
2020-06-17 04:53:45 +0200

    adaptive loss

--- loss_layer.py
+++ loss_layer.py
@@ -14,9 +14,8 @@ class LossLayer(nn.Module):
     def forward(self, contrastive_loss, cross_entropy_loss, contrastive_ratio):
         x = self.weights[0][0]
         y = self.weights[0][1]
-        x.to(device)
-        y.to(device)
+        x.to(self.device)
+        y.to(self.device)
         loss = (contrastive_ratio * torch.pow(contrastive_loss,x)) + ((1-contrastive_ratio) * torch.pow(cross_entropy_loss,y))
         return loss
 
-LossLayer()
